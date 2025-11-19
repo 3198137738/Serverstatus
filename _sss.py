13215a16b9ce0b84d1165187282694422e6d602c
Alexander Lubotzky <103321491+3198137738@users.noreply.github.com>
2025-11-19 19:23:56 +0800

    Update _sss.py

--- _sss.py
+++ _sss.py
@@ -13,7 +13,7 @@ import subprocess
 import uuid
 
 CONFIG_FILE = "config.json"
-GITHUB_RAW_URL = "https://raw.githubusercontent.com/3198137738/Serverstatus"
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/3198137738/Serverstatus/main"
 IP_URL = "https://api.ipify.org"
 
 jjs = {}
